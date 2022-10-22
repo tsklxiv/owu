a5d69bef52d21f0ea129c18fe85689383c020e7b
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-10-22 16:41:24 +0700

    Add missing return for verb/identifier cases in eval()

--- owu.py
+++ owu.py
@@ -153,7 +153,7 @@ def eval(x, env=global_env):
         args = [eval(exp, env) for exp in x["v"][1:]]
         print(f"Operator: {op}, Arguments: {args}")
         if op["t"] == 3 or op["t"] == 4:
-            handle_verbs(op, args, env)
+            return handle_verbs(op, args, env)
         else:
             return x
     else:             # Literals
