4798569267cde233edd14fbaa9469d845e5d24c2
Dang Hoang Tuan <danghoangtuan526@proton.me>
2022-10-23 16:46:18 +0700

    Readd the unexpected ] case in parseVal()

--- owu.py
+++ owu.py
@@ -111,6 +111,8 @@ def parseVal(code, pos):
         # Here we need to pop() the list to remove the remaining ]
         lst.pop()
         return ol(lst), pos
+    elif current == "]":
+        return os("Unexpected ]"), pos + 1
     elif current == "\"":        # Strings
         pos += 1
         s = ""
