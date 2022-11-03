cac59c5c677c3f968c33933f0ca8e6ab7a65b7b3
Dang Hoang Tuan <tsukii@disroot.org>
2022-11-03 20:34:47 +0700

    Specify the `p` (print) operator to just print out the value instead of the object

--- owu.py
+++ owu.py
@@ -80,7 +80,7 @@ def init_env():
         "~": lambda x: noto(x[0]),
         "&": lambda x: ando(x[0], x[1]),
         "|": lambda x: reverse(x[0]),
-        "p": lambda x: printo(x),
+        "p": lambda x: printo(o["v"] for o in x),
         "or": lambda x: oro(x[0], x[1]),
         "max": lambda x: reduce(maxo, x),
         "min": lambda x: reduce(mino, x),
