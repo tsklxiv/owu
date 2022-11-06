9f9e61ee4dc27e3f4b0474e6bb2507345f69d4ca
Dang Hoang Tuan <tsukii@disroot.org>
2022-11-06 16:26:59 +0700

    The function main() for handling CLI also need the argument `tree`

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from owu import run, repl
 @click.command()
 @click.argument("files", type=click.STRING, nargs=-1)
 @click.option("tree", "-t", type=click.BOOL, default=False, help="Print parsed tree of each line before evaling")
-def main(files):
+def main(files, tree):
     if len(files) == 0:
         repl()
     else:
         for file in files:
             with open(file, "r") as f:
-                run(f.read())
+                run(f.read(), tree)
 
 if __name__ == "__main__":
     main()
